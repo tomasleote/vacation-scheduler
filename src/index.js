b9578fbee9f14678f38e51967bcef5c6df6cf596
tomas.leote <tomas.leote@gmail.com>
2026-03-06 20:14:13 +0000

    fix(pwa): auto-reload on service worker update
    Add onUpdate callback to service worker registration so that when a
    new deployment is detected, the waiting service worker immediately
    activates and the page reloads. Prevents users from being stuck on
    stale cached versions after deployments.

--- src/index.js
+++ src/index.js
@@ -36,4 +36,12 @@ if (rootElement.hasChildNodes()) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    const waitingWorker = registration.waiting;
+    if (waitingWorker) {
+      waitingWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+    window.location.reload();
+  },
+});
